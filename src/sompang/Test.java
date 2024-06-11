9ffc29a302a93bb1f5bcba3e2eebbbac6beb2c7f
Jeremy-Fe <snrn221@naver.com>
2024-06-11 21:21:32 +0900

    동현이 풀 받기전

--- src/sompang/Test.java
+++ src/sompang/Test.java
@@ -2,8 +2,14 @@ package sompang;
 
 public class Test {
 	public static void main(String[] args) {
+		String name = "신우철";
+		int age = 25;
+		
+		System.out.println(name);
+		System.out.println(age);
 		System.out.println("신우철 대기중");
 		System.out.println("동현이 테스트");
+		System.out.println("충돌을 내보겠습니다.");
 		System.out.println();
 		System.out.println();
 		// 커밋 버전 3
