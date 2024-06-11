6f01632047eabc78656f0d6e11f886032bde8373
Jeremy-Fe <snrn221@naver.com>
2024-06-11 20:54:05 +0900

    initial commit 오류 해결 : (테스트 중임)

---
+++ src/sompang/Test.java
@@ -0,0 +1,13 @@
+package sompang;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("신우철 대기중");
+		System.out.println("동현이 테스트");
+		System.out.println();
+		System.out.println();
+		// 커밋 버전 3
+		System.out.println();
+		// 커믹 버전 4
+	}
+}
