5ff8c5ec17162c2b2b177706709ebd821f42b11a
솜팡이바보 <@DESKTOP-99PGQED>
2024-06-11 21:09:55 +0900

    동 commit  테스트

--- src/sompang/Test.java
+++ src/sompang/Test.java
@@ -2,6 +2,13 @@ package sompang;
 
 public class Test {
 	public static void main(String[] args) {
+		String name = "이동현 테스트";
+		String names = "신우철 테스트";
+		
+		System.out.println(name);
+		System.out.println(name);
+		System.out.println(names);
+		
 		System.out.println("신우철 대기중");
 		System.out.println("동현이 테스트");
 		System.out.println();
@@ -9,5 +16,6 @@ public class Test {
 		// 커밋 버전 3
 		System.out.println();
 		// 커믹 버전 4
+		
 	}
 }
